36e134507970d2fb561b50a1d88c36e6aa854bec
lemamon <demmarinho@gmail.com>
2021-04-01 16:17:39 -0300

    feat: check current selected to set class

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,36 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 function Header() {
+  const [selected, select] = useState(window.location.pathname);
+  const history = useHistory();
+
+  useEffect(() => {
+    return history.listen((location) => {
+      select(location.pathname);
+    });
+  }, [history]);
+
+  const isSelected =(option:string) => {
+    if(selected === "/" && option === "home") return  "selected"
+    else return selected.includes(option) ? "selected": ""
+  }
+
   return (
     <header>
       <nav>
         <ul>
-          <li>
+          <li className={isSelected("home")}>
             <Link to="/" aria-label="home">
               Home
             </Link>
           </li>
-          <li>
+          <li className={isSelected("about")}>
             <Link to="/about" aria-label="about">
               About
             </Link>
           </li>
-          <li>
+          <li className={isSelected("work")}>
             <Link to="/work" aria-label="work">
               Work
             </Link>
