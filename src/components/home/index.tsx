ed0514ab9f34f378bdda1d781922dc2f229906d4
lemamon <demmarinho@gmail.com>
2021-04-01 15:49:35 -0300

    feat: added container

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,14 +3,16 @@ import homeBanner from "../../assets/images/banner_home.png";
 function Home() {
   return (
     <section className="home">
-      <div className="title-container">
-        <h2 className="title">
-          Deyvid <b>Marinho</b>
-        </h2>
-        <h5 className="subtitle">Software Engineer</h5>
-      </div>
+      <div className="container">
+        <div className="title-container">
+          <h2 className="title">
+            Deyvid <b>Marinho</b>
+          </h2>
+          <h5 className="subtitle">Software Engineer</h5>
+        </div>
 
-      <img className="banner" alt="cat banner" src={homeBanner} />
+        <img className="banner" alt="cat banner" src={homeBanner} />
+      </div>
     </section>
   );
 }
