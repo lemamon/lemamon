fcb9ecf3ebbf702bee1c418a75368f9cdcb873ae
lemamon <demmarinho@gmail.com>
2021-03-31 22:46:48 -0300

    feat: added about component

---
+++ src/components/about/index.tsx
@@ -0,0 +1,39 @@
+function About() {
+  return (
+    <section className="about">
+      <div className="resume">
+        <h3>About me</h3>
+        <p>
+          Proactive and responsible professional, with the ability to learn with
+          easy. Knowledgeable with web and mobile technologies. And also with
+          Agile development methodologies. An enthusiast of new and upcoming
+          Technologies.
+        </p>
+      </div>
+
+      <div className="contact">
+        <h3>Contact</h3>
+        <p>
+          Interested in working together? Let's chat! Email me
+          <b><a className="email" href="mailto:demmarinho@gmail.com">{" "}demmarinho@gmail.com</a></b>
+        </p>
+      </div>
+
+      <div className="skills">
+        <h3>Skills (aka alphabet soup)</h3>
+        <p>
+          JavaScript, React, Android, iOS, ReactNative, Axe, Pyhton,C/C++, Java,
+          Python, C#, PHP, Clojure, Office Package, AutoCad, Unity 3D, Git,
+          Gitlab, MatLab/Octave, Windows, Linux Firebase, Firebase Fuctions,
+          Polymer, Web-Components, Progressive Web-apps, Windows Phone, Entity
+          Framework, NodeJS, Cordova/PhoneGap, Scrum, Kaban, Agile
+          Methodology,Xamarin, TDD, jQuery, CSS, MDL, Materialize, OpenCV, HTML,
+          MySQL, MongoDB, npm, ExpressJS, AngularJS, Loopback, ReactJS, GraphQL,
+          Prisma, Apollo, Relay, Relay Modern, BulmaCSS, Firebase Firestore.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
