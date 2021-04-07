c8787820dbc960b2c24cfb0c369ae0a026ff736a
lemamon <demmarinho@gmail.com>
2021-04-07 13:25:47 -0300

    feat: created main wrapper

--- src/components/switcher/index.tsx
+++ src/components/switcher/index.tsx
@@ -4,19 +4,34 @@ import Home from "./../home";
 import Work from "./../work";
 import About from "./../about";
 
+import { Fragment } from "react";
+
+function Main() {
+  return (
+    <Fragment>
+      <Home />
+      <About />
+      <Work />
+    </Fragment>
+  );
+}
+
 function Switcher() {
   return (
-    <Switch>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/work">
-        <Work />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
+    <>
+      <Switch>
+        <Route path="#about">
+          <About />
+        </Route>
+        <Route path="#work">
+          <Work />
+        </Route>
+        <Route path="#">
+          <Home />
+        </Route>
+      </Switch>
+      <Main />
+    </>
   );
 }
 
