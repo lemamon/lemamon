980b45cbc129d38906ec81544827f0465cc03dce
lemamon <demmarinho@gmail.com>
2021-04-21 13:39:05 -0300

    feat: remove subsubtitle

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -7,7 +7,7 @@ function Home() {
             Deyvid <b>Marinho</b>
           </h2>
           <h5 className="subtitle">Software Engineer 👨🏻‍💻</h5>
-          <p className="subsubtitle">(a.k.a. dev de applicativozinho)</p>
+          {/* <p className="subsubtitle">(a.k.a. dev de applicativozinho)</p> */}
         </div>
         <video autoPlay muted loop className=" home-video">
           <source src="/assets/videos/home-video2.mp4" type="video/mp4" />
