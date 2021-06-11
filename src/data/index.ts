d0559a6038cbcb6e31e058de97f4986daebf1f17
lemamon <demmarinho@gmail.com>
2021-06-10 23:01:54 -0300

    feat: changed data location

---
+++ src/data/index.ts
@@ -0,0 +1,20 @@
+export const footerItems = [
+  {
+    link: "https://github.com/lemamon",
+    name: "Github",
+  },
+  {
+    link: "https://linkedin.com/in/lemamon",
+    name: "Linkedin",
+  },
+  {
+    link: "https://instagram.com/lemamon",
+    name: "Instagram",
+  },
+  {
+    link: "https://twitter.com/lemamon",
+    name: "Twitter",
+  },
+];
+
+export const headerItems = ["home", "about", "work"];
