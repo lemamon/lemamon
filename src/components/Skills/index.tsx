d3ba536e2b14cae4cdb052292746c2b634fce3d8
lemamon <demmarinho@gmail.com>
2021-06-10 22:59:13 -0300

    feat: created skills component

---
+++ src/components/Skills/index.tsx
@@ -0,0 +1,22 @@
+import Text from "../Text";
+import TextTitle from "../TextTitle";
+
+function Skills() {
+  return (
+    <div>
+      <TextTitle>Skills (aka alphabet soup) 🍜</TextTitle>
+      <Text>
+        JavaScript, React, Android, iOS, ReactNative, Axe, Pyhton,C/C++, Java,
+        Python, C#, PHP, Clojure, Office Package, AutoCad, Unity 3D, Git,
+        Gitlab, MatLab/Octave, Windows, Linux Firebase, Firebase Fuctions,
+        Polymer, Web-Components, Progressive Web-apps, Windows Phone, Entity
+        Framework, NodeJS, Cordova/PhoneGap, Scrum, Kaban, Agile Methodology,
+        Xamarin, TDD, jQuery, CSS, MDL, Materialize, OpenCV, HTML, MySQL,
+        MongoDB, npm, ExpressJS, AngularJS, Loopback, ReactJS, GraphQL, Prisma,
+        Apollo, Relay, Relay Modern, BulmaCSS, Firebase Firestore.
+      </Text>
+    </div>
+  );
+}
+
+export default Skills;
