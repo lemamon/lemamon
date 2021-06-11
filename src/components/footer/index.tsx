65e5195f6d7609a98ad01ce5870cccce13140a96
lemamon <demmarinho@gmail.com>
2021-06-10 22:55:55 -0300

    feat: added styled

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,24 +1,48 @@
-function Footer({isHome}:{isHome:boolean}) {
-  return (
-    <footer style={isHome ? { backgroundColor: "transparent" } : {}}>
-      <nav>
-        <ul>
-          <li>
-            <a href="https://github.com/lemamon">Github</a>
-          </li>
-          <li>
-            <a href="https://linkedin.com/in/lemamon">Linkedin</a>
-          </li>
-          <li>
-            <a href="https://instagram.com/lemamon">Instagram</a>
-          </li>
-          <li>
-            <a href="https://twitter.com/lemamon">Twitter</a>
-          </li>
-        </ul>
-      </nav>
-    </footer>
-  );
+import { FC } from "react";
+import styled from "styled-components";
+import { footerItems } from "../../data";
+
+const Contianer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  min-width: 100vw;
+  background-color: #fff;
+  @media (min-width: 1024px) {
+    nav > ul {
+      justify-content: flex-end;
+    }
+  }
+`;
+
+const List = styled.ul`
+  margin: 10px 0px 25px;
+`;
+
+interface ItemProps {
+  name: string;
+  link: string;
 }
+
+const Item: FC<ItemProps> = ({ name, link }) => {
+  return (
+    <li>
+      <a href={link}>{name}</a>
+    </li>
+  );
+};
+
+const Footer = () => {
+  return (
+    <Contianer>
+      <nav>
+        <List>
+          {footerItems.map((item) => (
+            <Item key={item.name} {...item} />
+          ))}
+        </List>
+      </nav>
+    </Contianer>
+  );
+};
 
 export default Footer;
