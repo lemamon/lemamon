206ed52e4e812b44265653fb9600008c8de1c98d
lemamon <demmarinho@gmail.com>
2021-06-10 22:57:29 -0300

    feat: created link button styled

---
+++ src/components/LinkButton/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const LinkButton = styled.a`
+  text-decoration: none;
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export default LinkButton;
