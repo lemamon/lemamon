fd078eca2bbe8dfaa501f24b68c42ff4a6ddbf78
lemamon <demmarinho@gmail.com>
2024-10-17 00:04:52 -0300

    Refactor ContactForm component to update contact button functionality

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,23 +1,13 @@
 import { Row, Col } from "antd";
 import { withTranslation } from "react-i18next";
 import { Slide } from "react-awesome-reveal";
-import { ContactProps, ValidationTypeProps } from "./types";
-import { useForm } from "../../common/utils/useForm";
-import validate from "../../common/utils/validationRules";
+import { ContactProps } from "./types";
 import { Button } from "../../common/Button";
 import Block from "../Block";
-import Input from "../../common/Input";
-import TextArea from "../../common/TextArea";
-import { ContactContainer, FormGroup, Span, ButtonContainer } from "./styles";
+import { ContactContainer, ButtonContainer } from "./styles";
+import { SvgIcon } from "../../common/SvgIcon";
 
 const Contact = ({ title, content, id, t }: ContactProps) => {
-  const { values, errors, handleChange, handleSubmit } = useForm(validate);
-
-  const ValidationType = ({ type }: ValidationTypeProps) => {
-    const ErrorMessage = errors[type as keyof typeof errors];
-    return <Span>{ErrorMessage}</Span>;
-  };
-
   return (
     <ContactContainer id={id}>
       <Row justify="space-between" align="middle">
@@ -28,40 +18,14 @@ const Contact = ({ title, content, id, t }: ContactProps) => {
         </Col>
         <Col lg={12} md={12} sm={24} xs={24}>
           <Slide direction="right" triggerOnce>
-            <FormGroup autoComplete="off" onSubmit={handleSubmit}>
-              <Col span={24}>
-                <Input
-                  type="text"
-                  name="name"
-                  placeholder="Your Name"
-                  value={values.name || ""}
-                  onChange={handleChange}
-                />
-                <ValidationType type="name" />
-              </Col>
-              <Col span={24}>
-                <Input
-                  type="text"
-                  name="email"
-                  placeholder="Your Email"
-                  value={values.email || ""}
-                  onChange={handleChange}
-                />
-                <ValidationType type="email" />
-              </Col>
-              <Col span={24}>
-                <TextArea
-                  placeholder="Your Message"
-                  value={values.message || ""}
-                  name="message"
-                  onChange={handleChange}
-                />
-                <ValidationType type="message" />
-              </Col>
+            <>
+              <SvgIcon src="contact-us.webp" width="100%" height="100%" />
               <ButtonContainer>
-                <Button name="submit">{t("Submit")}</Button>
+                <a href="mailto:lemamon.dev@gmail.com?subject=Contact%20From%20Website">
+                  <Button name="submit">{t("Contact Us")}</Button>
+                </a>
               </ButtonContainer>
-            </FormGroup>
+            </>
           </Slide>
         </Col>
       </Row>
