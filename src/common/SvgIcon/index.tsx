8a29abac441f6383f61c94819fa827f4d240d650
lemamon <demmarinho@gmail.com>
2024-10-17 12:03:53 -0300

    Refactor SvgIcon component to dynamically load SVG icons

--- src/common/SvgIcon/index.tsx
+++ src/common/SvgIcon/index.tsx
@@ -1,5 +1,18 @@
 import { SvgIconProps } from "../types";
+import { useState, useEffect } from "react";
 
-export const SvgIcon = ({ src, width, height }: SvgIconProps) => (
-  <img src={`/img/svg/${src}`} alt={src} width={width} height={height} />
-);
+export const SvgIcon = ({ src, width, height }: SvgIconProps) => {
+  const [iconSrc, setIconSrc] = useState<string | null>(null);
+
+  useEffect(() => {
+    const basePath = import.meta.env.BASE_URL || "";
+    const iconPath = `${basePath}src/assets/img/svg/${src}`;
+    setIconSrc(iconPath);
+  }, [src]);
+
+  return iconSrc ? (
+    <img src={iconSrc} alt={src} width={width} height={height} />
+  ) : (
+    <span>Loading...</span>
+  );
+};
