6a9847eee1e0d9c3c8d7ba16ed3b51a1d9ec70fa
lemamon <demmarinho@gmail.com>
2025-01-27 23:58:38 -0300

    Update ContentBlock to escape content values and add posts data export

--- src/components/ContentBlock/index.tsx
+++ src/components/ContentBlock/index.tsx
@@ -47,7 +47,7 @@ const ContentBlock = ({
           <Col lg={11} md={11} sm={11} xs={24}>
             <ContentWrapper>
               <h6>{t(title)}</h6>
-              <Content>{t(content)}</Content>
+              <Content>{t(content, { escapeValue: true })}</Content>
               {direction === "right" ? (
                 <ButtonWrapper>
                   {typeof button === "object" &&
