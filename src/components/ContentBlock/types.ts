eee086e786f7bec583141f74c8eddab855fd0904
lemamon <demmarinho@gmail.com>
2025-02-14 21:18:11 -0300

    feat: update ContentBlockProps to include onClick handler in button type

--- src/components/ContentBlock/types.ts
+++ src/components/ContentBlock/types.ts
@@ -1,18 +1,24 @@
 import { TFunction } from "react-i18next";
+
+export type ButttonType = {
+  title: string;
+  color?: string;
+  path: string;
+  onClick?: () => void;
+};
+
+export type Section = {
+  title: string;
+  content: string;
+  icon: string;
+};
+
 export interface ContentBlockProps {
   icon: string;
   title: string;
   content: string;
-  section?: {
-    title: string;
-    content: string;
-    icon: string;
-  }[];
-  button?: {
-    title: string;
-    color?: undefined | string;
-    path: string;
-  }[];
+  section?: Section[];
+  button?: ButttonType[];
   t: TFunction;
   id: string;
   direction: "left" | "right";
