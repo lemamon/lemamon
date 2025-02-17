50490083d2105250a4f8efd35bd98ab4b037e113
lemamon <demmarinho@gmail.com>
2025-02-17 20:16:14 -0300

    refactor: simplify Option and QuestionItemProps types by removing unused properties

--- src/components/QuestionItem/types.ts
+++ src/components/QuestionItem/types.ts
@@ -1,18 +1,12 @@
 export type Option = {
   id: string;
   parent?: string;
-  title: string;
-  text?: string;
   icon?: string;
-  question?: string;
   options?: Option[];
 };
 
 export type QuestionItemProps = {
-  questionIndex: number;
   questionId: string;
-  title: string;
-  question?: string;
   options?: Option[];
   selectedOptions: Record<string, string[]>;
   toggleSelection: (questionId: string, optionId: string) => void;
